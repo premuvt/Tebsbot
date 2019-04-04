b25f916986d591dc1518f8991cd1ebc15d4bacc1
Premraj C Ramankutty <premraj.cr@uvionicstech.com>
2019-04-05 00:31:27 +0530

    Apply leave view controller added

---
+++ Tebsbot/Tebsbot/ApplyLeaveViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ApplyLeaveViewController.swift
+//  Tebsbot
+//
+//  Created by Premraj C Ramankutty on 04/04/19.
+//  Copyright © 2019 Premraj C Ramankutty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ApplyLeaveViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        let logo = UIImage(named: "logo")
+        let imageView = UIImageView(image:logo)
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        let button = UIButton.init(type: .custom)
+        button.setImage(UIImage.init(named: "arrow"), for: UIControl.State.normal)
+        button.addTarget(self, action:#selector(ApplyLeaveViewController.backAction), for:.touchUpInside)
+        button.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30) //CGRectMake(0, 0, 30, 30)
+        let barButton = UIBarButtonItem.init(customView: button)
+        self.navigationItem.leftBarButtonItem = barButton
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    @objc func backAction() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
