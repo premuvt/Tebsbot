77be139f10dc5fe76a10007ab8d0ebb28db784ba
Premraj C Ramankutty <premraj.cr@uvionicstech.com>
2019-04-04 21:55:10 +0530

    initial commit

---
+++ Tebsbot/Tebsbot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tebsbot
+//
+//  Created by Premraj C Ramankutty on 04/04/19.
+//  Copyright © 2019 Premraj C Ramankutty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
