3c78195220756ff4e84e7230b5ac1b309b66186e
Adarsh <adarsh.kc@uvionicstech.com>
2019-04-27 03:01:45 +0530

    leave confirmatio Page

---
+++ Tebsbot/Tebsbot/ViewControllers/LeaveConfirmationViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LeaveConfirmationViewController.swift
+//  Tebsbot
+//
+//  Created by uvionics on 27/04/19.
+//  Copyright © 2019 Premraj C Ramankutty. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class LeaveConfirmationViewController: UIViewController {
+    
+    var synth:AVSpeechSynthesizer = AVSpeechSynthesizer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.speakText(message: "Your leave has been applied. Thank you.")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        synth.stopSpeaking(at: .immediate)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        navigationController?.navigationBar.backgroundColor = UIColor.white
+        let logo = UIImage(named: "logo")
+        let imageView = UIImageView(image:logo)
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        
+    }
+    
+    @IBAction func buttonOKClicked(sender:UIButton){
+        self.navigationController?.popToRootViewController(animated: true)
+        
+    }
+
+    
+    //MARK:- text to speech
+    func speakText(message:String) {
+        print("test to voice - ",message)
+        let utterance = AVSpeechUtterance(string: message)
+        utterance.voice = AVSpeechSynthesisVoice(language: NSLocale.current.identifier)
+        utterance.volume = 1.0
+        
+        self.synth.speak(utterance)
+    }
+}
+
